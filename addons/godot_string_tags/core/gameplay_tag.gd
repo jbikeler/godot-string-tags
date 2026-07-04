aad8ed9a863f7101205d4d1103c00b58837856bd
jbikeler <jbikeler@gmail.com>
2026-07-04 16:38:39 -0700

    Updated GameplayTag Class

--- addons/godot_string_tags/core/gameplay_tag.gd
+++ addons/godot_string_tags/core/gameplay_tag.gd
@@ -2,18 +2,37 @@
 class_name GameplayTag
 extends Resource
 
+
 @export var tag: String = ""
 
 
 ## Returns true if this tag matches the given string including hierarchy.
 ## e.g. matches("Character.Status") returns true if tag is "Character.Status.Burning"
-func matches(other: String) -> bool:
+func includes(other: String) -> bool:
 	return tag == other or tag.begins_with(other + ".")
 
 
 ## Returns true if this tag exactly matches the given string.
-func matches_exact(other: String) -> bool:
+func matches(other: String) -> bool:
 	return tag == other
+
+
+## Returns true if this tag matches the given gameplay tag including hierarchy.
+## Ex:
+## var example_gt_tag_1 : GameplayTag = Character.Status
+## var example_gt_tag_2 : GameplayTag = Character.Status.Burning
+## example_gt_tag_1.matches(example_gt_tag_2) returns true
+func matches_gt(other: GameplayTag) -> bool:
+	return tag == other.tag or tag.begins_with(other.tag + ".")
+
+
+## Returns true if this tag exactly matches the given string.
+## Ex:
+## var example_gt_tag_1 : GameplayTag = Character.Status
+## var example_gt_tag_2 : GameplayTag = Character.Status.Burning
+## example_gt_tag_1.matches(example_gt_tag_2) returns false
+func matches_gt_exact(other: GameplayTag) -> bool:
+	return tag == other.tag
 
 
 ## Returns a readable string of this tag.
